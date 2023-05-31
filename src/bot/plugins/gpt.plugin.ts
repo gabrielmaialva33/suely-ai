65ddf1680fbdc6c9cc49397b4b13f1f036304e95
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-05-31 20:22:40 -0300

    chore: fix sonarqube code

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -22,7 +22,7 @@ class OpenAI extends OpenAIApi {
   }
 
   private RandonCompletionRequest = {
-    model: 'text-davinci-003',
+    model: 'text-davinci-002',
     temperature: 1,
     max_tokens: 150,
     // randomize 1.5 2.0
