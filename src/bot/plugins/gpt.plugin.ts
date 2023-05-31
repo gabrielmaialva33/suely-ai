c1003ef25a077ed05e1411c882bdad69bd25ee8f
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-05-31 15:54:56 -0300

    feat: add menu item

--- src/bot/plugins/gpt.plugin.ts
+++ src/bot/plugins/gpt.plugin.ts
@@ -22,7 +22,7 @@ class OpenAI extends OpenAIApi {
   }
 
   private RandonCompletionRequest = {
-    model: 'text-davinci-002',
+    model: 'text-davinci-003',
     temperature: 1,
     max_tokens: 150,
     // randomize 1.5 2.0
@@ -30,7 +30,7 @@ class OpenAI extends OpenAIApi {
     // randomize 1.0 1.5
     presence_penalty: Math.random() * (1.5 - 1.0) + 1.0,
     //top_p: 0.3,
-    n: 1,
+    n: 3,
     //n: 1,
     stop: ['||'],
   } as CreateCompletionRequest
